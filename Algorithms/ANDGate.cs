21fbc55b38f26f2d8e72f7729a7b9c1257270557
Matt Bessler <matthew.bessler@cix.csi.cuny.edu>
2017-10-17 15:31:39 -0400

    Update ANDGate.cs

--- Algorithms/ANDGate.cs
+++ Algorithms/ANDGate.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace CSC_523_Game
 {
-    class ANDGate
+    class ANDGate : Gate
     {
+        public ANDGate(Variable [] vars) : base(vars)
+        {
+
+        }
+
+        public override bool getResult()
+        {
+            bool result = false;
+
+            foreach(Variable v in vars)
+            {
+                result = result && v.getTruthValue();
+            }
+
+            return result;
+        }
     }
 }
