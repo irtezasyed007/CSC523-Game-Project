fe2f0373fd2b1fd6077c25082dc0d718c9cd53a6
Matt Bess <matthew.bessler@cix.csi.cuny.edu>
2017-11-09 18:08:02 -0500

    pushing before merge to master

---
+++ TheCircuitry/Assets/Scripts/ParticleSystemSorting.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleSystemSorting : MonoBehaviour {
+
+    public ParticleSystem gameObject;
+	// Use this for initialization
+	void Start () {
+       gameObject.GetComponent<Renderer>().sortingLayerName = "Foreground";
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
