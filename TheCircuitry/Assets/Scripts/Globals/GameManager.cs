4f14932439bbd9e56e53a343696b3659c82509c4
irtezasyed007 <irteza.m.syed@gmail.com>
2017-11-28 15:09:04 -0500

    Merged relevant changes

---
+++ TheCircuitry/Assets/Scripts/Globals/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager Manager;
+    // Use this for initialization
+    private void Awake()
+    {
+        if (Manager == null)
+        {
+            DontDestroyOnLoad(gameObject);
+            Manager = this;
+        }
+        else if (Manager != null)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ TheCircuitry/Assets/Scripts/LineRenderer/DrawLines.cs
@@ -0,0 +1,59 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Put this script on a Camera
+public class DrawLines : MonoBehaviour
+{
+
+    // Fill/drag these in from the editor
+
+    // Choose the Unlit/Color shader in the Material Settings
+    // You can change that color, to change the color of the connecting lines
+    public Material lineMat;
+
+    public GameObject mainPoint;
+    public GameObject point1;
+    public GameObject point2;
+
+
+    // Connect all of the `points` to the `mainPoint`
+    void DrawConnectingLines()
+    {
+        if (mainPoint && point1 && point2)
+        {
+            // Loop through each point to connect to the mainPoint
+            //foreach (GameObject point in points)
+            //{
+                Vector3 mainPointPos = mainPoint.transform.position;
+                Vector3 point1Pos = point1.transform.position;
+                Vector3 point2Pos = point2.transform.position;
+
+                GL.Begin(GL.LINES);
+                lineMat.SetPass(0);
+                GL.Color(new Color(lineMat.color.r, lineMat.color.g, lineMat.color.b, lineMat.color.a));
+                GL.Vertex3(mainPointPos.x, mainPointPos.y, mainPointPos.z);
+                GL.Vertex3(point1Pos.x, point1Pos.y, point1Pos.z);
+                GL.End();
+
+                GL.Begin(GL.LINES);
+                lineMat.SetPass(0);
+                GL.Color(new Color(lineMat.color.r, lineMat.color.g, lineMat.color.b, lineMat.color.a));
+                GL.Vertex3(mainPointPos.x, mainPointPos.y, mainPointPos.z);
+                GL.Vertex3(point2Pos.x, point2Pos.y, point2Pos.z);
+                GL.End();
+            //}
+        }
+    }
+
+    // To show the lines in the game window whne it is running
+    void OnPostRender()
+    {
+        DrawConnectingLines();
+    }
+
+    // To show the lines in the editor
+    void OnDrawGizmos()
+    {
+        DrawConnectingLines();
+    }
+}
